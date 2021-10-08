baddb3d3caab218443d8255a541a2d9999be994c
SabbirHossain07 <sabbirsopnil07@gmail.com>
2021-10-08 13:53:18 +0600

    Initial Commit

---
+++ Carousel_Castom/Carousel_CastomApp.swift
@@ -0,0 +1,17 @@
+//
+//  Carousel_CastomApp.swift
+//  Carousel_Castom
+//
+//  Created by Sopnil Sohan on 8/10/21.
+//
+
+import SwiftUI
+
+@main
+struct Carousel_CastomApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
